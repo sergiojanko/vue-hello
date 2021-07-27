f23a596f5b4473e955e34f6a43a5715ce0df2bc1
sergiojanko <janko.sergio1998@gmail.com>
2021-07-27 15:27:36 +0200

    render-picture

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var root = new Vue({
     el: "#root",
     data: {
-        message : "Hello Boolean!"
+        message: "Hello Boolean!",
+        picture: "https://images.unsplash.com/photo-1626634913593-e5b88fd85627?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80",
     }
 });
